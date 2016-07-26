c9890ab00b859483dd230811d3cbcb0e9d0366ed
Tim Ruffing <tim.ruffing@mmci.uni-saarland.de>
2016-07-26 16:46:08 +0200

    Add Python wrapper for extension module

--- dicemix/solver/solver.cpp
+++ dicemix/solver/solver.cpp
@@ -166,16 +166,17 @@ int main(int argc, char* argv[])
  *
  * Solves the equation system
  *   forall 0 <= i < n. sum_{j=0}^{n-1} messages[j]^{i+1} = sums[i]
- * in the finite prime field F_p for messages[], and checks if my_message is a solution.
+ * in the finite prime field F_prime for messages[], and checks if my_message is in the solution.
  *
  * \param[out] out_messages    Array of n char buffers (allocated by caller) of length at least strlen(prime) + 1
  * \param[in]  prime           Prime of the finite field (not checked for primality)
  * \param[in]  my_message      Our own message
  * \param[in]  sums            Array of n power sums
- * \param[in]  n               Number of peers
+ * \param[in]  n               Number of peers, must be at least 2 and not larger than prime
  *
- * \retval 0                   Success, the outputs are stored as hexadecimal strings in messages[].
- * \retval RET_INVALID         The system is not properly solvable or my_message is not a solution.
+ * \retval 0                   Success, the solution vector is stored as hexadecimal strings in messages[],
+ *                             sorted in ascending numerical order.
+ * \retval RET_INVALID         sums is not a proper array of power sums or my_message is not in the solution.
  * \retval RET_INPUT_ERROR     Illegal input values.
  * \retval RET_INTERNAL_ERROR  An internal error occured.
  */
